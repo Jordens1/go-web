29034a1e4a0a22bfa279d22ca681fbc6998d1807
Jordens1 <1285389644@qq.com>
2022-03-24 22:49:31 +0800

    p5完

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"html/template"
 	"net/http"
 	"time"
@@ -12,6 +13,19 @@ type Article struct {
 	Title   string `json:"title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
+}
+
+// get的参数会根据表单的值进行对应
+type People struct {
+	Name string `json:"name" form:"name"`
+	Age  string `json:"age" form:"age"`
+	Sex  string `json:"sex" form:"sex"`
+}
+
+// xml数据进行解析绑定
+type Article2 struct {
+	Title   string `json:"title" xml:"title"`
+	Content string `json:"content" xml:"content"`
 }
 
 // 模板函数的实现
@@ -36,6 +50,79 @@ func main() {
 	// get方法
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "值是：%v", "你好")
+	})
+
+	// get方法,传值
+	r.GET("/para", func(c *gin.Context) {
+		name := c.Query("name")
+		age := c.DefaultQuery("age", "18")
+
+		c.JSON(http.StatusOK, gin.H{
+			"name": name,
+			"age":  age,
+		})
+	})
+
+	// get动态路由
+	r.GET("/para/:uid", func(c *gin.Context) {
+		uid := c.Param("uid")
+
+		c.JSON(http.StatusOK, gin.H{
+			"uid": uid,
+		})
+	})
+
+	// get方法,显示表单
+	r.GET("/get/form", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "guest/index.html", gin.H{})
+
+	})
+	// post，数据字段和index.html的表单对应
+	r.POST("/post/doAddUser", func(c *gin.Context) {
+		username := c.PostForm("username")
+		pass := c.DefaultPostForm("passwd", "woshishei")
+		id := c.DefaultPostForm("id", "12")
+		c.JSON(http.StatusOK, gin.H{
+			"name": username,
+			"pass": pass,
+			"id":   id,
+		})
+	})
+
+	// 获取表单的数据绑定到结构体上
+	r.GET("/get/form/bind", func(c *gin.Context) {
+		p := &People{}
+		if err := c.ShouldBind(&p); err == nil {
+			c.JSON(http.StatusOK, p)
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"err": err.Error(),
+			})
+		}
+	})
+	r.POST("/post/doAddUser/withStruct", func(c *gin.Context) {
+		p := &People{}
+		if err := c.ShouldBind(&p); err == nil {
+			c.JSON(http.StatusOK, p)
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+		}
+	})
+
+	// xml数据的绑定
+	r.POST("/post/xml/bind", func(c *gin.Context) {
+		art := &Article2{}
+
+		b, _ := c.GetRawData()
+		if err := xml.Unmarshal(b, &art); err == nil {
+			c.JSON(http.StatusOK, art)
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+		}
 	})
 
 	// json数据
